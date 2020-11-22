9763083676b0bb00e9edac9a894f783b61f77db1
mvmedeiros <mvbmedeiros@gmail.com>
2020-11-21 22:21:37 -0300

    Atualiza: i) tela de filmes e séries.

--- Projeto_Integrador_Grupo04/app/src/main/java/com/projetointegrador/grupo04/movies_series/view/MoviesSeriesFragment.kt
+++ Projeto_Integrador_Grupo04/app/src/main/java/com/projetointegrador/grupo04/movies_series/view/MoviesSeriesFragment.kt
@@ -1,49 +1,54 @@
 package com.projetointegrador.grupo04.movies_series.view
 
 import android.os.Bundle
+import android.view.*
+import android.widget.ImageView
 import androidx.fragment.app.Fragment
-import android.view.LayoutInflater
-import android.view.View
-import android.view.ViewGroup
+import android.widget.Toast
 import androidx.core.os.bundleOf
 import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProvider
 import androidx.lifecycle.ViewModelProviders
 import androidx.navigation.findNavController
+import androidx.navigation.fragment.findNavController
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 import com.projetointegrador.grupo04.R
 import com.projetointegrador.grupo04.movies_series.model.MovieModel
 import com.projetointegrador.grupo04.movies_series.repository.MovieRepository
 import com.projetointegrador.grupo04.movies_series.viewmodel.MovieListViewModel
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MoviesSeriesFragment : Fragment() {
-    lateinit var movieListView: View
-    lateinit var movieListViewModel: MovieListViewModel
+    private lateinit var _movieListView: View
+    private lateinit var _movieListViewModel: MovieListViewModel
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        movieListView = inflater.inflate(R.layout.fragment_series_movies, container, false)
-        movieListViewModel = ViewModelProvider(this,
+        _movieListView = inflater.inflate(R.layout.fragment_series_movies, container, false)
+        _movieListViewModel = ViewModelProvider(this,
             MovieListViewModel.ListRestaurantListViewModelFactory(MovieRepository())
         ).get(MovieListViewModel::class.java)
 
-        movieListViewModel.movieList.observe(this, Observer {
+        _movieListViewModel.movieList.observe(this, Observer {
             createTrendingList(it)
             createAiringList(it)
             createPopularList(it)
             createUpcomingList(it)
         })
 
-        movieListViewModel.getList()
-        return movieListView
+       _movieListViewModel.getList()
+
+        setUserOptions()
+
+        return _movieListView
     }
 
     fun createTrendingList(restaurant: List<MovieModel>) {
-        val recyclerView = movieListView.findViewById<RecyclerView>(R.id.rvMediaTrending)
+        val recyclerView = _movieListView.findViewById<RecyclerView>(R.id.rvMediaTrending)
         val manager = LinearLayoutManager(context,LinearLayoutManager.HORIZONTAL, false)
 
         val customAdapter = MovieListAdapter(restaurant) {
@@ -57,7 +62,7 @@ class MoviesSeriesFragment : Fragment() {
     }
 
     fun createAiringList(restaurant: List<MovieModel>) {
-        val recyclerView = movieListView.findViewById<RecyclerView>(R.id.rvMediaAiring)
+        val recyclerView = _movieListView.findViewById<RecyclerView>(R.id.rvMediaAiring)
         val manager = LinearLayoutManager(context,LinearLayoutManager.HORIZONTAL, false)
 
         val customAdapter = MovieListAdapter(restaurant) {
@@ -71,7 +76,7 @@ class MoviesSeriesFragment : Fragment() {
     }
 
     fun createPopularList(restaurant: List<MovieModel>) {
-        val recyclerView = movieListView.findViewById<RecyclerView>(R.id.rvMediaPopular)
+        val recyclerView = _movieListView.findViewById<RecyclerView>(R.id.rvMediaPopular)
         val manager = LinearLayoutManager(context,LinearLayoutManager.HORIZONTAL, false)
 
         val customAdapter = MovieListAdapter(restaurant) {
@@ -85,7 +90,7 @@ class MoviesSeriesFragment : Fragment() {
     }
 
     fun createUpcomingList(restaurant: List<MovieModel>) {
-        val recyclerView = movieListView.findViewById<RecyclerView>(R.id.rvMediaUpcoming)
+        val recyclerView = _movieListView.findViewById<RecyclerView>(R.id.rvMediaUpcoming)
         val manager = LinearLayoutManager(context,LinearLayoutManager.HORIZONTAL, false)
 
         val customAdapter = MovieListAdapter(restaurant) {
@@ -95,6 +100,12 @@ class MoviesSeriesFragment : Fragment() {
             setHasFixedSize(true)
             layoutManager = manager
             adapter = customAdapter
+        }
+    }
+
+    private fun setUserOptions() {
+        _movieListView.findViewById<ImageView>(R.id.imgSeriesDetailsBack).setOnClickListener {
+            Toast.makeText(context,"OPA OLHA AE",Toast.LENGTH_SHORT).show()
         }
     }
 }
