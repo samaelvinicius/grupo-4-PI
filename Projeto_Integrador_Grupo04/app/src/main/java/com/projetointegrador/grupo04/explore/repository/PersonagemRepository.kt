201cf2a4b003a619de0956c986a04f0871156bee
Samael Vinícius <70551168+samaelvinicius@users.noreply.github.com>
2020-12-11 00:25:00 -0300

    Delete PersonagemRepository.kt

--- Projeto_Integrador_Grupo04/app/src/main/java/com/projetointegrador/grupo04/explore/repository/PersonagemRepository.kt
+++
@@ -1,7 +0,0 @@
-package com.projetointegrador.grupo04.explore.repository
-
-class PersonagemRepository {
-    private val client = PersonagemEndpoint.endpoint
-
-    suspend fun obterLista() = client.obterLista()
-}
